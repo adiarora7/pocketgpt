10cd44ee8897ba293ed77a1ae8b08e759bbc33ef
adiarora7 <96757084+adiarora7@users.noreply.github.com>
2023-02-16 23:18:47 -0800

    General Working version with greeting support

--- app.py
+++ app.py
@@ -26,12 +26,12 @@ def chatgpt():
     """Check incoming number"""
     
     inb_num = request.form['From']
-    #if verify_number(inb_num, data) == False:
-        #resp = MessagingResponse()
-       # resp.message("Hi! You're not currently signed up for PocketGPT. To access PocketGPT, sign up for free at https://pocket-gpt.com/free-signup/")
+    if verify_number(inb_num, data) == False:
+        resp = MessagingResponse()
+        resp.message("Hi! You're not currently signed up for PocketGPT. To access PocketGPT, sign up for free at https://pocket-gpt.com/free-signup/")
         #return str(resp)
     
-    if int(inb_num)==17787107707:
+    else:
         """get incoming message"""
         inb_msg = request.form['Body'].lower()
         print(inb_msg)
@@ -57,15 +57,15 @@ def chatgpt():
 
         return str(resp)
     
-    elif verify_number(inb_num, data) == True:
+    """ elif verify_number(inb_num, data) == True:
         resp = MessagingResponse()
         resp.message("Hi, we appreciate you using PocketGPT! We appreciate your patience as we refine the model. PocketGPT will be up soon.")
-        return str(resp)
+        return str(resp) """
     
-    else:
+    """ else:
         resp = MessagingResponse()
         resp.message("Hi! You're not currently signed up for PocketGPT. To be notified about the release of PocketGPT, sign up at https://pocket-gpt.com/free-signup/")
-        return str(resp)
+        return str(resp) """
 
 if __name__ == "__main__":
     app.run(debug=True)
